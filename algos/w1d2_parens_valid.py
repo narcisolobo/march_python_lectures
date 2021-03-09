2c21611883a48380fce41d71f17a8c05dc34aa16
Narciso Lobo <narcisolobo@gmail.com>
2021-03-08 22:30:32 -0600

    added algo and lecture set up for week 1 day 2

---
+++ algos/w1d2_parens_valid.py
@@ -0,0 +1,3 @@
+# Parens Valid
+# Create a function that, given an input string, returns a boolean whether parentheses in that string are valid. Given input "y(3(p)p(3)r)s", return true. Given "n(0(p)3", return false. Given "n)0(t(0)k", return false.
+
---
+++ lectures/w1d1_morning_loops_and_functions.py
@@ -0,0 +1,55 @@
+# Python Stack - Day 2
+
+# Loops and Functions
+
+# for-in loops with range() function
+
+# for-in loops over lists with range() function
+
+# for-in loops over lists without range() function
+
+# for-in loops through dictionaries
+
+# While loops
+
+# Else in for and while loops
+
+shang = {
+    'real_name': 'Shang-Chi',
+    'code_name': 'Shang-Chi, Master of Kung Fu',
+    'origin': 'Raised to be an assassin.',
+    'arch_enemy': 'Fu Manchu'
+}
+
+marvel_superheroes = [
+    {
+        'real_name': 'Shang-Chi',
+        'code_name': 'Shang-Chi, Master of Kung Fu',
+        'origin': 'Raised to be an assassin.',
+        'arch_enemy': 'Fu Manchu'
+    },
+    {
+        'real_name': 'Peter Parker',
+        'code_name': 'Spider-Man',
+        'origin': 'Radioactive spider.',
+        'arch_enemy': 'Green Goblin'
+    },
+    {
+        'real_name': 'Luke Cage',
+        'code_name': 'Luke Cage',
+        'origin': 'Failed prison experiment.',
+        'arch_enemy': 'Diamondback'
+    },
+    {
+        'real_name': 'Matt Murdock',
+        'code_name': 'Daredevil',
+        'origin': 'Radioactive isotopes.',
+        'arch_enemy': 'Kingpin'
+    },
+    {
+        'real_name': 'Steve Rogers',
+        'code_name': 'Captain America',
+        'origin': 'Super soldier serum.',
+        'arch_enemy': 'The Red Skull'
+    },
+]
