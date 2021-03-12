3daf1ec8321d5fc969e8d1059a55228c2e2a5276
Narciso Lobo <narcisolobo@gmail.com>
2021-03-12 16:57:24 -0600

    added afternoon code

---
+++ lectures/superheroes/data.py
@@ -0,0 +1,35 @@
+def list_of_superheroes():
+    superheroes = [
+        {
+            'real_name': 'Shang-Chi',
+            'code_name': 'Shang-Chi, Master of Kung Fu',
+            'origin': 'Raised to be an assassin.',
+            'arch_enemy': 'Fu Manchu'
+        },
+        {
+            'real_name': 'Peter Parker',
+            'code_name': 'Spider-Man',
+            'origin': 'Radioactive spider.',
+            'arch_enemy': 'Green Goblin'
+        },
+        {
+            'real_name': 'Luke Cage',
+            'code_name': 'Luke Cage',
+            'origin': 'Failed prison experiment.',
+            'arch_enemy': 'Diamondback'
+        },
+        {
+            'real_name': 'Matt Murdock',
+            'code_name': 'Daredevil',
+            'origin': 'Radioactive isotopes.',
+            'arch_enemy': 'Kingpin'
+        },
+        {
+            'real_name': 'Steve Rogers',
+            'code_name': 'Captain America',
+            'origin': 'Super soldier serum.',
+            'arch_enemy': 'The Red Skull'
+        },
+    ]
+
+    return superheroes
--- lectures/superheroes/server.py
+++ lectures/superheroes/server.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, redirect
+from data import list_of_superheroes
+
+back_end_superheroes = list_of_superheroes()
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', front_end_superheroes=back_end_superheroes)
 
 if __name__ == '__main__':
     app.run(debug=True)
