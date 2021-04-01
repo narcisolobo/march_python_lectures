d805c6b0eef28e07496a6db32f4ebf0c45d195e2
Narciso Lobo <narcisolobo@gmail.com>
2021-04-01 13:10:54 -0500

    added recursive GCF

---
+++ algos/recursion/gcf.py
@@ -0,0 +1,40 @@
+# Greatest Common Factor / Greatest Common Divisor
+# The highest number that divides evenly into two or more numbers.
+
+# 12, 8
+# Factors of 12 = 1, 2, 3, 4, 6, 12
+# Factors of 8 = 1, 2, 4, 8
+# 4 is the highest number that divides evenly into 12 and 8.
+
+# Euclid's Algorithm:
+# Given two whole numbers where a is greater than b, do the division a ÷ b = c with remainder R.
+# Replace a with b, replace b with R and repeat the division.
+# Repeat step 2 until R=0.
+# When R=0, the divisor, b, in the last equation is the greatest common factor, GCF.
+
+# 12 % 8 = 4
+# 8 % 4 = 0
+
+# Recursive algorithm for Greatest Common Factor (GCF)
+# Write a recursive function that takes in two integers and returns the greatest common factor.
+
+# Remember, a recursive function should have:
+# 1. Base Case
+# 2. Forward Progress
+# 3. A Call to the Function Inside the Function
+
+def rGCF(a, b):
+    if (b == 0):
+        return a
+    else:
+        return rGCF(b, a % b)
+
+# Predicted call stack:
+# rGCF(12, 81)
+# rGCF(81, 12)
+# rGCF(12, 9)
+# rGCF(9, 3)
+# rGCF(3, 0)
+# 3 is the predicted GCF.
+
+print(rGCF(12, 81))
